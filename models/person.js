fa3a34434081ce34f7e5e150afbf0ede21b2e96c
dvs110 <satidevang2001@gmail.com>
2023-04-13 22:11:07 +0530

    first commit

---
+++ models/person.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+const personSchema = new Schema({
+    email: { type: String, required: true, unique: true },
+    fname: { type: String, required: true },
+    lname: { type: String, required: true },
+    password: { type: String, required: true },
+    aboutme: { type: String, default: 'Add something about you.' },
+    phone: { type: String },
+    linkedin: { type: String, default: 'Linkedin' },
+    github: { type: String, default: 'Github' },
+    facebook: { type: String, default: 'Facebook' },
+    twitter: { type: String, default: 'Twitter' },
+    instagram: { type: String, default: 'Instagram' },
+    website: { type: String, default: 'Website' },
+    image: { type: String, default: 'https://www.cipherschools.com/static/media/Cipherschools_icon@2x.3b571d743ffedc84d039.png' },
+    follower: {
+        type: String,
+        default: "0"
+    },
+    highesteducation: { type: String, default: 'Secondary' },
+    whatdoudocurrently: { type: String, default: 'College Student' }
+});
+export default mongoose.model("Car", personSchema);
