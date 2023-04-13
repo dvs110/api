54b4026fa3650807579e6a276843d27119007531
dvs110 <satidevang2001@gmail.com>
2023-04-13 23:13:42 +0530

    first commit

--- index.js
+++ index.js
@@ -26,9 +26,7 @@ const connect = async () => {
 mongoose.connection.on('disconnected', () => { //if mongodb got disconnected
     console.log("mongodb disconnected");
 });
-app.get("/ash", async (req, res, next) => {
-    res.send(100)
-})
+
 
 app.post('/form', async (req, res, next) => {
     const salt = bcrypt.genSaltSync(10);
